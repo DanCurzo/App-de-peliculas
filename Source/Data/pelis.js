0fa558f72a7f3d0cdeae1b098d96bf296b70fc87
DanCurzo <benitez.e.joaquin@gmail.com>
2022-09-19 18:30:58 -0300

    objetos

--- Source/Data/pelis.js
+++ Source/Data/pelis.js
@@ -1,1 +1,28 @@
-const peliculas = ["Shrek","Captain America","Spider-Man","Iron Man","Avengers"];
+const peliculas = 
+[
+    {
+        nombre: "Shrek",
+        categoria: "comedia",
+        duracion: "1:30hs"
+    },
+    {
+        nombre: "Spider-Man",
+        categoria: "Accion",
+        duracion: "2:10hs"
+    }
+    {
+        nombre: "Iron Man",
+        categoria: "Accion",
+        duracion: "1:50hs"
+    }
+    {
+        nombre: "Megamente",
+        categoria: "Comedia",
+        duracion: "1:30hs"
+    }
+    {
+        nombre: "Los Simpsons",
+        categoria: "Accion",
+        duracion: "2:10hs"
+    }
+];
