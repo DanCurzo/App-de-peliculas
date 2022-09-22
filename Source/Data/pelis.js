6e9cbbf8ecabce24f528fa1204938cf61bc8485b
DanCurzo <benitez.e.joaquin@gmail.com>
2022-09-22 16:25:03 -0300

    imagenes

--- Source/Data/pelis.js
+++ Source/Data/pelis.js
@@ -3,26 +3,36 @@ const peliculas =
     {
         nombre: "Shrek",
         categoria: "comedia",
-        duracion: "1:30hs"
+        duracion: "1:30hs",
+        description:"Shrek es un gran ogro verde que vive en un pantano.",
+        images: "/images/shrek-image.jpg"
     },
     {
-        nombre: "Spider-Man",
+        nombre: "El Hombre Araña 2",
         categoria: "Accion",
-        duracion: "2:10hs"
-    }
+        duracion: "2:10hs",
+        description:"El atormentado Peter Parker lucha contra un científico siniestro que utiliza sus tentáculos mecánicos con fines destructivos.",
+        images: "/images/spider-man-image.jpg"
+    },
     {
         nombre: "Iron Man",
         categoria: "Accion",
-        duracion: "1:50hs"
-    }
+        duracion: "1:50hs",
+        description:"La trama gira en torno a Tony Stark, un empresario e ingeniero, que construye un exoesqueleto motorizado y se convierte en Iron Man, un superhéroe tecnológicamente avanzado.",
+        images: "/images/iron-man-image.jpg"
+    },
     {
         nombre: "Megamente",
         categoria: "Comedia",
-        duracion: "1:30hs"
-    }
+        duracion: "1:30hs",
+        description:"A pesar de que él es el supervillano más brillante que ha tenido el mundo, Megamente es el menos exitoso. Frustrado una y otra vez por el heroico Metro Man, Megamente se sorprende cuando logra superar a un enemigo de tanto tiempo. pero sin Metro Man, Megamente no tiene ningún propósito en la vida, por lo que crea a un nuevo oponente, quien rápidamente decide que es más divertido ser el malo que el héroe.",
+        images: "/images/megamente-image.jpg"
+    },
     {
-        nombre: "Los Simpsons",
+        nombre: "Los Simpson",
         categoria: "Accion",
-        duracion: "2:10hs"
+        duracion: "2:10hs",
+        description:"La combinación de Homero, su nuevo puerco mascota, y un silo lleno de excremento podrían provocar un desastre que amenace no solo a Springfield, sino al mundo entero. Una muchedumbre enojada llega a la casa de los Simpson y divide a la familia. Con el destino de la Tierra en juego, Homero se prepara para intentar redimirse con la intención de salvar al mundo y ganarse el perdón de Marge.",
+        images: "/images/los-simpson-image.jpg"
     }
 ];
